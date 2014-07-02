6a96915e34be8e87488a890341c9744b01a5b9db
Jacob Niedzwiecki <jake@jacob-n.com>
2014-07-01 23:05:06 -0400

    Finished heartbeat test

--- server.js
+++ server.js
@@ -5,36 +5,34 @@ var http = require('http'),
  	port = 8000,
   	id = 0,
   	timer = new NanoTimer(),
-  	clients = new Array();
+  	clients = [];
 
-console.log("finished instantiation");
-/*
-var server = http.createServer(function(req, res){
-	var self = this;
- 	res.writeHead(200, {
+var app = express();
+
+var server = app.listen(port, function(){
+	console.log('Streaming events on port ' + port);
+});
+
+app.get('/', function(req, res){
+	res.writeHead(200, {
  		'Transfer-Encoding': 'chunked', 
  		'Content-Type': 'text/event-stream',
  		'Cache-Control': 'no-cache',
         'Connection': 'keep-alive'
     });
     res.write("retry: 10000\n");
-}).listen(port);
-
-console.dir(server);
-
-timer.setInterval(emitHeartbeat, '', '1s');
-
-console.log('Streaming events on port ' + port);
+    clients.push(res);
+});
 
 function emitHeartbeat(){
-	var event = 'event: message\ndata: {"id": ' + (++id) + ', "body":"' + (new Date().getTime()) + '", "author_id": 1, "conversation_id": 1}\n\n';
+	var event = 'event: message\ndata: {"id": ' + (++id) + ', "body":"' + (new Date().getTime()) + '"}\n\n';
 	//console.log(event);
 	if(clients != 'undefined' && clients.length > 0){
-		var i = 0;
-		for(client in clients){
-			//.write(event);
+		for(var i = 0; i < clients.length; i++){
+			clients[i].write(event);
 			console.log("broadcast to client " + i);
-			i++;
 		}
 	}
-}*/
+}
+
+timer.setInterval(emitHeartbeat, '', '1s');
