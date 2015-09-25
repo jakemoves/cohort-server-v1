0eb064352401599bd40da449d9a2113b8304f6f8
Luke Garwood <luke_gar@hotmail.com>
2015-09-24 20:21:59 -0400

    Added amended flux site

--- public/js/fluxdelux.js
+++ public/js/fluxdelux.js
@@ -1,3 +1,6 @@
+var info = document.getElementById("info");
+var b1 = document.getElementById("b1");
+
 var audio = document.createElement('audio');
 audio.src = 'http://webspace.ocad.ca/~lg14ig/media/corners.mp3';
 
@@ -127,3 +130,9 @@ for(var i =0; i < allAudio.length; i ++){
 
 
 };
+
+$("#b1").click(function(){
+  info.innerHTML = "Fantastic, you are now in queue. Please wait for audio instructions.";
+  b1.innerHTML = "You are now standying by!"
+
+})
